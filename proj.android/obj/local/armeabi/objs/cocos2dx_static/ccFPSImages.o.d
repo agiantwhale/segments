a309e9275d02ea7e06ca0802d5cbd239a45b198e
IJ Lee <smashthewindow@gmail.com>
2013-08-05 21:57:07 +0900

    Removed obj dir.

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/ccFPSImages.c \
- /Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/ccFPSImages.h
-
-/Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/iljae/Cocos2D-x/cocos2d-x-2.1.4/segment/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
